cd99696f057c7b7840118430723c98e0dd56bd89
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-03-30 13:27:10 +0300

    Created Statistics component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Section from './Section/Section';
 import Profile from './Profile/Profile';
 import user from './Profile/user.json';
+import Statistics from './Statistics/Statistics';
+import statData from './Statistics/stats-data.json';
 
 const App = () => (
   <>
+    <Statistics title="Upload statistics" stats={statData} />
     <Section title="User profile">
       {/* <Profile
         avatar={user.avatar}
