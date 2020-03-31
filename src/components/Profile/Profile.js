72d400a13c42a3ba9b7b6c9a00fe01eb52eae6d1
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-03-31 13:39:50 +0300

    Added CSS styles for Profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
 
-const Profile = ({ name, tag, location, avatar, stats }) => (
-  <div className="profile">
-    <div className="description">
-      <img src={avatar} alt="user avatar" className="avatar" />
-      <p className="name">{name}</p>
-      <p className="tag">@{tag}</p>
-      <p className="location">{location}</p>
+const Profile = ({ name, tag, location, avatar, alt, stats }) => (
+  <div className={styles.profile}>
+    <div className={styles.description}>
+      <img src={avatar} alt={alt} className={styles.avatar} />
+      <p className={styles.name}>{name}</p>
+      <p className={styles.tag}>@{tag}</p>
+      <p className={styles.location}>{location}</p>
     </div>
 
-    <ul className="stats">
-      <li>
-        <span className="label">Followers</span>
-        <span className="quantity">{stats.followers}</span>
+    <ul className={styles.list}>
+      <li className={styles.item}>
+        <span className={styles.label}>Followers</span>
+        <span className={styles.quantity}>{stats.followers}</span>
       </li>
-      <li>
-        <span className="label">Views</span>
-        <span className="quantity">{stats.views}</span>
+      <li className={styles.item}>
+        <span className={styles.label}>Views</span>
+        <span className={styles.quantity}>{stats.views}</span>
       </li>
-      <li>
-        <span className="label">Likes</span>
-        <span className="quantity">{stats.likes}</span>
+      <li className={styles.item}>
+        <span className={styles.label}>Likes</span>
+        <span className={styles.quantity}>{stats.likes}</span>
       </li>
     </ul>
   </div>
@@ -30,6 +31,7 @@ const Profile = ({ name, tag, location, avatar, stats }) => (
 Profile.defaultProps = {
   avatar:
     'https://i.pinimg.com/originals/a0/40/66/a04066a2d1fcf25df39c599e093995c8.jpg',
+  alt: 'user avatar',
 };
 
 Profile.propTypes = {
