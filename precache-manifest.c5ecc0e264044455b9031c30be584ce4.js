dc4ee6e52b6b5c21b992ea9b07fee192ca3059ee
Yevhen Konyk <yevhen.konyk@gmail.com>
2020-03-31 17:31:49 +0300

    Updates

---
+++ precache-manifest.c5ecc0e264044455b9031c30be584ce4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d592fceea3949a89eaf83e4f06d0c432",
+    "url": "/react-hw-01-components/index.html"
+  },
+  {
+    "revision": "cb7b2bdbca8897129178",
+    "url": "/react-hw-01-components/static/css/main.27923b35.chunk.css"
+  },
+  {
+    "revision": "f2a157895a6ba3513d9e",
+    "url": "/react-hw-01-components/static/js/2.34a61159.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-01-components/static/js/2.34a61159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7b2bdbca8897129178",
+    "url": "/react-hw-01-components/static/js/main.16350f2d.chunk.js"
+  },
+  {
+    "revision": "a4733b9d2429552f2f32",
+    "url": "/react-hw-01-components/static/js/runtime-main.6fb6e3e8.js"
+  }
+]);
